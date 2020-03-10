d53dfe6e360fec4b3129ecb7ff2d2008bee1bb9d
Ricardo Teixeira <rteixxeira86@gmail.com>
2020-03-10 20:38:25 +0000

    Add ServiceErrorException

---
+++ StockManager.Types/ServiceErrorException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StockManager.Types {
+  public class ServiceErrorException : ArgumentException {
+    public List<ErrorType> Errors { get; private set; }
+
+    public ServiceErrorException(OperationErrorsList operationErrors) {
+      this.Errors = operationErrors.ErrorsList;
+    }
+  }
+}
