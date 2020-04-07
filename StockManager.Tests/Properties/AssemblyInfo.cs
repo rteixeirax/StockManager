aa4ec153e92ecbce00287069c9584afeb68db2e2
Ricardo Teixeira <rteixxeira86@gmail.com>
2020-04-07 19:45:39 +0100

    Add tests library

---
+++ StockManager.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StockManager.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StockManager.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7ca69608-48c0-4ce7-a4e9-bd7107026acb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StockManager.Tests/Services/UserServiceTests.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using StockManager.Storage.Contracts;
+using StockManager.Storage.Models;
+using Tynamix.ObjectFiller;
+using StockManager.Services.Services;
+using System.Threading.Tasks;
+using StockManager.Types.Types;
+
+namespace StockManager.Tests.Services {
+  /// <summary>
+  /// User service tests
+  /// </summary>
+  [TestClass]
+  public class UserServiceTests {
+    private readonly Mock<IUserRepository> userRepo;
+
+    public UserServiceTests() {
+      this.userRepo = new Mock<IUserRepository>();
+    }
+
+    /// <summary>
+    /// Test user create
+    /// </summary>
+    [TestMethod]
+    public async Task ShouldCreateUserAsync() {
+      // Arrange 
+      User user = new Filler<User>().Create();
+      this.userRepo.Setup(repo => repo.AddUserAsync(It.IsAny<User>()));
+
+      // Act 
+      var userService = new UserService(this.userRepo.Object);
+      await userService.CreateUserAsync(user);
+
+      // Assert 
+      this.userRepo.Verify(repo => repo.AddUserAsync(It.IsAny<User>()), Times.Once);
+    }
+
+    /// <summary>
+    /// Test user create fails
+    /// </summary>
+    [TestMethod]
+    public async Task ShouldThrowOperationExceptionWhenCreateUserAsyncFails() {
+      // Arrange 
+      User user = new Filler<User>().Create();
+      OperationErrorException operationErrorException = this.CreateOperationErrorException();
+
+      userRepo.Setup(broker => broker.AddUserAsync(It.IsAny<User>()))
+        .Throws(operationErrorException);
+
+      try {
+        // Act 
+        var userService = new UserService(userRepo.Object);
+        await userService.CreateUserAsync(user);
+      } catch (OperationErrorException ex) {
+        // Assert 
+        this.userRepo.Verify(broker => broker.AddUserAsync(It.IsAny<User>()), Times.Once);
+        Assert.AreEqual(ex.Errors.Count, 1);
+      }
+    }
+
+    /// <summary>
+    /// Test user update
+    /// </summary>
+    //[TestMethod]
+    //public async Task ShouldEditUserAsync() {
+    //  // Arrange 
+    //  User user = new Filler<User>().Create();
+    //  this.userRepo.Setup(repo => repo.AddUserAsync(It.IsAny<User>()));
+    //  //this.userRepo.Setup(repo => repo.UpdateUser(It.IsAny<User>()));
+
+    //  // Act 
+    //  var userService = new UserService(this.userRepo.Object);
+    //  await userService.EditUserAsync(user);
+
+    //  // Assert 
+    //  this.userRepo.Verify(repo => repo.SaveDbChangesAsync(), Times.Once);
+    //}
+
+    /// <summary>
+    /// Create operation error exception
+    /// </summary>
+    private OperationErrorException CreateOperationErrorException() {
+      OperationErrorsList errorsList = new OperationErrorsList();
+      errorsList.AddError(new MnemonicString().GetValue(), new MnemonicString().GetValue());
+
+      OperationErrorException operationErrorException = new OperationErrorException(errorsList);
+
+      return operationErrorException;
+    }
+  }
+}
