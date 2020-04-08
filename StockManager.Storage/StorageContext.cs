e2e0f462b50274e6455b57324f19472b743b1232
Ricardo Teixeira <rteixxeira86@gmail.com>
2020-04-08 21:23:22 +0100

    Wip: User tests

--- StockManager.Storage/StorageContext.cs
+++ StockManager.Storage/StorageContext.cs
@@ -10,14 +10,26 @@ using System.Threading.Tasks;
 namespace StockManager.Storage {
   public class StorageContext : DbContext {
 
-    public StorageContext() : base() {
+    public StorageContext(): base() {
+      // Run the migrations when the DB is instantiated
+      //this.Database.Migrate();
+    }
+
+    /// <summary>
+    /// This contructor is to configure the test database
+    /// </summary>
+    public StorageContext(DbContextOptions<StorageContext> options)
+      : base(options) {
       // Run the migrations when the DB is instantiated
       this.Database.Migrate();
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
       base.OnConfiguring(optionsBuilder);
-      optionsBuilder.UseSqlite(@"Data Source=.\App.db.sqlite");
+
+      if (!optionsBuilder.IsConfigured) {
+        optionsBuilder.UseSqlite(@"Data Source=.\App.db.sqlite");
+      }
     }
 
     /// <summary>
--- StockManager.Tests/Services/UserServiceTests.cs
+++ StockManager.Tests/Services/UserServiceTests.cs
@@ -6,6 +6,11 @@ using Tynamix.ObjectFiller;
 using StockManager.Services.Services;
 using System.Threading.Tasks;
 using StockManager.Types.Types;
+using StockManager.Storage.Repositories;
+using StockManager.Services.Contracts;
+using StockManager.Storage;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace StockManager.Tests.Services {
   /// <summary>
@@ -13,10 +18,12 @@ namespace StockManager.Tests.Services {
   /// </summary>
   [TestClass]
   public class UserServiceTests {
-    private readonly Mock<IUserRepository> userRepo;
+    private readonly StorageContext db;
+    private readonly IUserService userService;
 
     public UserServiceTests() {
-      this.userRepo = new Mock<IUserRepository>();
+      this.db = new StorageConfiguration().StorageContext;
+      this.userService = new UserService(new UserRepository(this.db));
     }
 
     /// <summary>
@@ -25,68 +32,169 @@ namespace StockManager.Tests.Services {
     [TestMethod]
     public async Task ShouldCreateUserAsync() {
       // Arrange 
-      User user = new Filler<User>().Create();
-      this.userRepo.Setup(repo => repo.AddUserAsync(It.IsAny<User>()));
+      User newUser = new User() {
+        Username = "manel",
+        Password = "123",
+        RoleId = 1, // admin
+      };
+
+      // TODO: When the newUser is sent to the DB
+      // After saveChanges, the newUser got the password updated
+      // I can avoid get the user becouse his data already change?!!
 
       // Act 
-      var userService = new UserService(this.userRepo.Object);
-      await userService.CreateUserAsync(user);
+      await this.userService.CreateUserAsync(newUser);
+      User dbUser = await this.db.Users
+        .Where(x => x.Username.ToLower() == newUser.Username.ToLower())
+        .FirstOrDefaultAsync();
 
       // Assert 
-      this.userRepo.Verify(repo => repo.AddUserAsync(It.IsAny<User>()), Times.Once);
+      Assert.AreEqual(dbUser.Username, newUser.Username);
+      Assert.AreEqual(dbUser.RoleId, newUser.RoleId);
+      Assert.IsTrue(BCrypt.Net.BCrypt.Verify(newUser.Password, dbUser.Password));
+      Assert.IsNotNull(dbUser.CreatedAt);
+      Assert.IsNotNull(dbUser.UpdatedAt);
     }
 
     /// <summary>
-    /// Test user create fails
+    /// Test user create fails (username already exists)
     /// </summary>
     [TestMethod]
-    public async Task ShouldThrowOperationExceptionWhenCreateUserAsyncFails() {
+    public async Task ShoulFailCreateUserWithExistingUsername() {
       // Arrange 
-      User user = new Filler<User>().Create();
-      OperationErrorException operationErrorException = this.CreateOperationErrorException();
-
-      userRepo.Setup(broker => broker.AddUserAsync(It.IsAny<User>()))
-        .Throws(operationErrorException);
+      User user = new User() {
+        Username = "admin",
+        Password = "123",
+        RoleId = 1, // admin
+      };
 
       try {
         // Act 
-        var userService = new UserService(userRepo.Object);
-        await userService.CreateUserAsync(user);
+        await this.userService.CreateUserAsync(user);
+
+        Assert.Fail("It should have thrown an OperationErrorExeption");
       } catch (OperationErrorException ex) {
         // Assert 
-        this.userRepo.Verify(broker => broker.AddUserAsync(It.IsAny<User>()), Times.Once);
         Assert.AreEqual(ex.Errors.Count, 1);
+        Assert.AreEqual(ex.Errors[0].Field, "Username");
+        Assert.AreEqual(ex.Errors[0].Error, "This username already exist.");
       }
     }
 
     /// <summary>
-    /// Test user update
+    /// Test user create fails (no username and password sent)
     /// </summary>
-    //[TestMethod]
-    //public async Task ShouldEditUserAsync() {
-    //  // Arrange 
-    //  User user = new Filler<User>().Create();
-    //  this.userRepo.Setup(repo => repo.AddUserAsync(It.IsAny<User>()));
-    //  //this.userRepo.Setup(repo => repo.UpdateUser(It.IsAny<User>()));
+    [TestMethod]
+    public async Task ShoulFailCreateUserNoUsernameAndPassword() {
+      // Arrange 
+      User user = new User() { RoleId = 1 };
 
-    //  // Act 
-    //  var userService = new UserService(this.userRepo.Object);
-    //  await userService.EditUserAsync(user);
+      try {
+        // Act 
+        await this.userService.CreateUserAsync(user);
 
-    //  // Assert 
-    //  this.userRepo.Verify(repo => repo.SaveDbChangesAsync(), Times.Once);
-    //}
+        Assert.Fail("It should have thrown an OperationErrorExeption");
+      } catch (OperationErrorException ex) {
+        // Assert 
+        Assert.AreEqual(ex.Errors.Count, 2);
+        Assert.AreEqual(ex.Errors[0].Field, "Username");
+        Assert.AreEqual(ex.Errors[0].Error, "This field is required.");
+        Assert.AreEqual(ex.Errors[1].Field, "Password");
+        Assert.AreEqual(ex.Errors[1].Error, "This field is required.");
+      }
+    }
 
     /// <summary>
-    /// Create operation error exception
+    /// Test user edit
     /// </summary>
-    private OperationErrorException CreateOperationErrorException() {
-      OperationErrorsList errorsList = new OperationErrorsList();
-      errorsList.AddError(new MnemonicString().GetValue(), new MnemonicString().GetValue());
+    [TestMethod]
+    public async Task ShouldEditUserAsync() {
+      // Arrange 
+      await this.userService.CreateUserAsync(new User() {
+        Username = "manelito",
+        Password = "123",
+        RoleId = 2, // user
+      });
 
-      OperationErrorException operationErrorException = new OperationErrorException(errorsList);
+      User mockUser = await this.db.Users
+       .Where(x => x.Username.ToLower() == "manelito")
+       .FirstOrDefaultAsync();
 
-      return operationErrorException;
+      // Act
+      User updatedUser = new User() {
+        UserId = mockUser.UserId,
+        RoleId = 1,
+        Username = "manelito updated",
+        Password = "321",
+      };
+
+      await this.userService.EditUserAsync(updatedUser);
+      User dbUser = await this.userService.GetUserByIdAsync(updatedUser.UserId);
+
+      // Assert
+      Assert.AreEqual(dbUser.UserId, updatedUser.UserId);
+      Assert.AreEqual(dbUser.Username, "manelito updated");
+      Assert.IsTrue(BCrypt.Net.BCrypt.Verify(updatedUser.Password, dbUser.Password));
+      Assert.AreEqual(dbUser.RoleId, 1);
     }
+
+    /// <summary>
+    /// Test user edit fails (username already exists)
+    /// </summary>
+    [TestMethod]
+    public async Task ShoulFailEditUserWithExistingUsername() {
+      // Arrange 
+      await this.userService.CreateUserAsync(new User() {
+        Username = "manelito",
+        Password = "123",
+        RoleId = 2, // user
+      });
+
+      User mockUser = await this.db.Users
+       .Where(x => x.Username.ToLower() == "manelito")
+       .FirstOrDefaultAsync();
+
+      try {
+        // Act
+        User updatedUser = new User() {
+          UserId = mockUser.UserId,
+          RoleId = mockUser.RoleId,
+          Username = "admin",
+        };
+
+        await this.userService.EditUserAsync(updatedUser);
+
+        Assert.Fail("It should have thrown an OperationErrorExeption");
+      } catch (OperationErrorException ex) {
+        // Assert 
+        Assert.AreEqual(ex.Errors.Count, 1);
+        Assert.AreEqual(ex.Errors[0].Field, "Username");
+        Assert.AreEqual(ex.Errors[0].Error, "This username already exist.");
+      }
+    }
+
+    /// <summary>
+    /// Test user change password
+    /// </summary>
+
+    /// <summary>
+    /// Test user change password fails (invalid current password)
+    /// </summary>
+
+    /// <summary>
+    /// Test user change password fails (no current password and newPassword sent)
+    /// </summary>
+
+    /// <summary>
+    /// Test user authentication
+    /// </summary>
+
+    /// <summary>
+    /// Test user authentication fails (no username and password sent)
+    /// </summary>
+
+    /// <summary>
+    /// Test user authentication fails (bad username and password combination)
+    /// </summary>
   }
 }
