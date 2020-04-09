950087d3ca4edf712e5b9c72de92ec2532eee0d0
Ricardo Teixeira <rteixxeira86@gmail.com>
2020-04-09 20:43:19 +0100

    Finish user service tests

--- StockManager.Tests/Services/UserServiceTests.cs
+++ StockManager.Tests/Services/UserServiceTests.cs
@@ -6,6 +6,8 @@ using StockManager.Types.Types;
 using StockManager.Storage.Repositories;
 using StockManager.Services.Contracts;
 using StockManager.Storage;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StockManager.Tests.Services {
   /// <summary>
@@ -22,10 +24,10 @@ namespace StockManager.Tests.Services {
     }
 
     /// <summary>
-    /// Test user create
+    /// Should create user
     /// </summary>
     [TestMethod]
-    public async Task ShouldCreateUserAsync() {
+    public async Task ShouldCreateUser() {
       // Arrange 
       User newUser = new User() {
         Username = "manel",
@@ -45,10 +47,53 @@ namespace StockManager.Tests.Services {
     }
 
     /// <summary>
-    /// Test user create fails (username already exists)
+    /// Should get all users
     /// </summary>
     [TestMethod]
-    public async Task ShoulFailCreateUserWithExistingUsername() {
+    public async Task ShouldGetAllUsers() {
+      // Arrange 
+      User newUser = new User() {
+        Username = "manel",
+        Password = "123",
+        RoleId = 1, // admin
+      };
+
+      // Act 
+      await this.userService.CreateUserAsync(newUser);
+      IEnumerable<User> users = await this.userService.GetUsersAsync();
+
+      // Assert 
+      Assert.AreEqual(users.Count(), 2);
+      Assert.AreEqual(users.ElementAt(0).Username, "Admin");
+      Assert.AreEqual(users.ElementAt(1).Username, newUser.Username);
+    }
+
+    /// <summary>
+    /// Should search user for username
+    /// </summary>
+    [TestMethod]
+    public async Task ShouldSearchUserForUsername() {
+      // Arrange 
+      User adminUser = await this.userService.GetUserByIdAsync(1);
+      User newUser = new User() {
+        Username = "manel",
+        Password = "123",
+        RoleId = 1, // admin
+      };
+
+      // Act 
+      IEnumerable<User> users = await this.userService.GetUsersAsync(adminUser.Username);
+
+      // Assert 
+      Assert.AreEqual(users.Count(), 1);
+      Assert.AreEqual(users.ElementAt(0).Username, adminUser.Username);
+    }
+
+    /// <summary>
+    /// Should fail create user with a existing username
+    /// </summary>
+    [TestMethod]
+    public async Task ShouldFailCreateUser_ExistingUsername() {
       // Arrange 
       User newUser = new User() {
         Username = "admin",
@@ -70,10 +115,10 @@ namespace StockManager.Tests.Services {
     }
 
     /// <summary>
-    /// Test user create fails (no username and password sent)
+    /// Should faail create user - no usernmae and password sent
     /// </summary>
     [TestMethod]
-    public async Task ShoulFailCreateUserNoUsernameAndPassword() {
+    public async Task ShoulFailCreateUser_NoUsernameAndPasswordSent() {
       // Arrange 
       User newUser = new User() { RoleId = 1 };
 
@@ -93,10 +138,10 @@ namespace StockManager.Tests.Services {
     }
 
     /// <summary>
-    /// Test user edit
+    /// Should edit user
     /// </summary>
     [TestMethod]
-    public async Task ShouldEditUserAsync() {
+    public async Task ShouldEditUser() {
       // Arrange 
       User mockUser = new User() {
         Username = "manelito",
@@ -125,10 +170,10 @@ namespace StockManager.Tests.Services {
     }
 
     /// <summary>
-    /// Test user edit fails (username already exists)
+    /// Should fail edit user - existing username
     /// </summary>
     [TestMethod]
-    public async Task ShoulFailEditUserWithExistingUsername() {
+    public async Task ShouldFailEditUser_ExistingUsername() {
       // Arrange 
       User mockUser = new User() {
         Username = "manelito",
@@ -158,27 +203,164 @@ namespace StockManager.Tests.Services {
     }
 
     /// <summary>
-    /// Test user change password
+    /// Should delete user
     /// </summary>
+    [TestMethod]
+    public async Task ShouldDeleteUser() {
+      // Arrange 
+      User mockUser = new User() {
+        Username = "manelito",
+        Password = "123",
+        RoleId = 2, // user
+      };
+      User loggedInUser = await this.userService.GetUserByIdAsync(1);
+      await this.userService.CreateUserAsync(mockUser);
+
+      // Act
+      await this.userService.DeleteUserAsync(new int[] { mockUser.UserId }, loggedInUser.UserId);
+      User dbUser = await this.userService.GetUserByIdAsync(mockUser.UserId);
+
+      // Assert
+      Assert.IsNull(dbUser);
+    }
 
     /// <summary>
-    /// Test user change password fails (invalid current password)
+    /// Should fali delete user - loggedInUser
     /// </summary>
+    [TestMethod]
+    public async Task ShouldFailDeleteUser_LoggedInUser() {
+      // Arrange 
+      User loggedInUser = await this.userService.GetUserByIdAsync(1);
+
+      try {
+        // Act
+        await this.userService.DeleteUserAsync(new int[] { loggedInUser.UserId }, loggedInUser.UserId);
+
+        Assert.Fail("It should have thrown an OperationErrorExeption");
+      } catch (OperationErrorException ex) {
+        // Assert 
+        Assert.AreEqual(ex.Errors.Count, 1);
+        Assert.AreEqual(ex.Errors[0].Field, "LoggedInUserId");
+        Assert.AreEqual(ex.Errors[0].Error, "You can't delete yourself.");
+      }
+    }
 
     /// <summary>
-    /// Test user change password fails (no current password and newPassword sent)
+    /// Should change password
     /// </summary>
+    [TestMethod]
+    public async Task ShouldChangePassword() {
+      // Arrange 
+      User adminUser = await this.userService.GetUserByIdAsync(1);
+
+      // Act 
+      await this.userService.ChangePasswordAsync(adminUser.UserId, "admin", "newPassword");
+
+      // Assert 
+      Assert.IsTrue(BCrypt.Net.BCrypt.Verify("newPassword", adminUser.Password));
+    }
 
     /// <summary>
-    /// Test user authentication
+    ///  Should fail change password - no valid current password sent
     /// </summary>
+    [TestMethod]
+    public async Task ShouldFailChangePassword_NoValidCurrentPasswordSent() {
+      // Arrange 
+      User adminUser = await this.userService.GetUserByIdAsync(1);
+
+      try {
+        // Act
+        await this.userService.ChangePasswordAsync(adminUser.UserId, "invalidCurrentPass", "newPassword");
+
+        Assert.Fail("It should have thrown an OperationErrorExeption");
+      } catch (OperationErrorException ex) {
+        // Assert 
+        Assert.AreEqual(ex.Errors.Count, 1);
+        Assert.AreEqual(ex.Errors[0].Field, "CurrentPassword");
+        Assert.AreEqual(ex.Errors[0].Error, "Invalid password.");
+      }
+    }
 
     /// <summary>
-    /// Test user authentication fails (no username and password sent)
+    /// Should fail change password - no current and new password sent
     /// </summary>
+    [TestMethod]
+    public async Task ShouldFailChangePassword_NoCurrentAndNewPasswordSent() {
+      // Arrange 
+      User adminUser = await this.userService.GetUserByIdAsync(1);
+
+      try {
+        // Act
+        await this.userService.ChangePasswordAsync(adminUser.UserId, "", "");
+
+        Assert.Fail("It should have thrown an OperationErrorExeption");
+      } catch (OperationErrorException ex) {
+        // Assert 
+        Assert.AreEqual(ex.Errors.Count, 2);
+        Assert.AreEqual(ex.Errors[0].Field, "CurrentPassword");
+        Assert.AreEqual(ex.Errors[0].Error, "This field is required.");
+        Assert.AreEqual(ex.Errors[1].Field, "NewPassword");
+        Assert.AreEqual(ex.Errors[1].Error, "This field is required.");
+      }
+    }
 
     /// <summary>
-    /// Test user authentication fails (bad username and password combination)
+    /// Should authenticate user
     /// </summary>
+    [TestMethod]
+    public async Task ShouldAuthenticateUser() {
+      // Arrange 
+      User adminUser = await this.userService.GetUserByIdAsync(1);
+
+      // Act 
+      await this.userService.AuthenticateAsync(adminUser.Username, "admin");
+
+      // Assert 
+      Assert.IsNotNull(adminUser.LastLogin);
+    }
+
+    /// <summary>
+    /// Should fail authenticate user - no username and password sent
+    /// </summary>
+    [TestMethod]
+    public async Task ShouldFailAuthenticateUser_NoUsernameAndPasswordSent() {
+      // Arrange 
+      // nothing to arrange...
+
+      try {
+        // Act
+        await this.userService.AuthenticateAsync("", "");
+
+        Assert.Fail("It should have thrown an OperationErrorExeption");
+      } catch (OperationErrorException ex) {
+        // Assert 
+        Assert.AreEqual(ex.Errors.Count, 2);
+        Assert.AreEqual(ex.Errors[0].Field, "Username");
+        Assert.AreEqual(ex.Errors[0].Error, "This field is required.");
+        Assert.AreEqual(ex.Errors[1].Field, "Password");
+        Assert.AreEqual(ex.Errors[1].Error, "This field is required.");
+      }
+    }
+
+    /// <summary>
+    /// Should fail authenticate user - bad username and password combination
+    /// </summary>
+    [TestMethod]
+    public async Task ShouldFailAuthenticateUser_BadUsernameAndPasswordCombination() {
+      // Arrange 
+      User adminUser = await this.userService.GetUserByIdAsync(1);
+
+      try {
+        // Act
+        await this.userService.AuthenticateAsync(adminUser.Username, "notMyPassword");
+
+        Assert.Fail("It should have thrown an OperationErrorExeption");
+      } catch (OperationErrorException ex) {
+        // Assert 
+        Assert.AreEqual(ex.Errors.Count, 1);
+        Assert.AreEqual(ex.Errors[0].Field, "Generic");
+        Assert.AreEqual(ex.Errors[0].Error, "Invalid username and password combination.");
+      }
+    }
   }
 }
