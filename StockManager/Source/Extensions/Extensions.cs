19b8a5e7f4764f765b86f8a9e1a4af8c4de4ea9b
Ricardo Teixeira <rteixxeira86@gmail.com>
2020-07-08 22:53:37 +0100

    Code formating

--- StockManager/Source/Extensions/Extensions.cs
+++ StockManager/Source/Extensions/Extensions.cs
@@ -4,33 +4,33 @@ using System;
 
 namespace StockManager.Source.Extensions
 {
-    public static class Extensions
+  public static class Extensions
+  {
+    public static string ConcatMovementString(this StockMovement stockMovement)
     {
-        public static string ConcatMovementString(this StockMovement stockMovement)
-        {
-            string fromLocation = (stockMovement.FromLocation != null)
-               ? stockMovement.FromLocation.Name
-               : stockMovement.FromLocationName;
+      string fromLocation = (stockMovement.FromLocation != null)
+         ? stockMovement.FromLocation.Name
+         : stockMovement.FromLocationName;
 
-            string toLocation = (stockMovement.ToLocation != null)
-              ? stockMovement.ToLocation.Name
-              : stockMovement.ToLocationName;
+      string toLocation = (stockMovement.ToLocation != null)
+        ? stockMovement.ToLocation.Name
+        : stockMovement.ToLocationName;
 
-            string concat = $"{Phrases.StockMovementFrom}: {(fromLocation ?? "---")}"
-              + Environment.NewLine
-              + $"{Phrases.StockMovementTo}: {toLocation}";
+      string concat = $"{Phrases.StockMovementFrom}: {(fromLocation ?? "---")}"
+        + Environment.NewLine
+        + $"{Phrases.StockMovementTo}: {toLocation}";
 
-            return concat;
-        }
-
-        public static string ShortDateWithTime(this DateTime? date)
-        {
-            if (date == null)
-            {
-                return "";
-            }
-
-            return date?.ToString("dd MMM yyyy, HH:mm:ss");
-        }
+      return concat;
     }
+
+    public static string ShortDateWithTime(this DateTime? date)
+    {
+      if (date == null)
+      {
+        return "";
+      }
+
+      return date?.ToString("dd MMM yyyy, HH:mm:ss");
+    }
+  }
 }
