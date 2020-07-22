f89cdefb1ee7af224522e361706c0e8d66843958
Ricardo Teixeira <rteixxeira86@gmail.com>
2020-07-22 22:04:11 +0100

    Create a BaseRepository class and the IBaseRepository contract

---
+++ StockManager.Database/Source/Contracts/IBaseRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace StockManager.Database.Source.Contracts
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        Task AddAsync(TEntity entity);
+
+        void Remove(TEntity entity);
+
+        Task<TEntity> GetByIdAsync(int id);
+
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        Task<int> CountAsync();
+
+        Task<IEnumerable<TEntity>> FindAllAsync(Expression<Func<TEntity, bool>> expression);
+
+        Task<TEntity> FindOneAsync(Expression<Func<TEntity, bool>> expression);
+    }
+}
