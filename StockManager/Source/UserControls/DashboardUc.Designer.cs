220cc3956802ca0fa764c5bb4d285bb404d011fa
Ricardo Teixeira <rteixxeira86@gmail.com>
2020-08-13 17:39:27 +0100

    Wip: Add the DashboardUc for the user non admin

---
+++ StockManager/Source/UserControls/DashboardUc.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace StockManager.Source.UserControls
+{
+    partial class DashboardUc
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // DashboardUc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Font = new System.Drawing.Font("Courier New", 9.75F);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "DashboardUc";
+            this.Size = new System.Drawing.Size(860, 643);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
