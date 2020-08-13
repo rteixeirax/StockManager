ac245e1ebb48254ef9a29650c97a295aea9e917d
Ricardo Teixeira <rteixxeira86@gmail.com>
2020-08-13 18:50:02 +0100

    More wip on the DashboardUc

--- StockManager/Source/UserControls/DashboardUc.Designer.cs
+++ StockManager/Source/UserControls/DashboardUc.Designer.cs
@@ -28,21 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.lbLocation = new System.Windows.Forms.Label();
+            this.cbLocations = new System.Windows.Forms.ComboBox();
+            this.ColumnProductLocationId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnProducRef = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnProductName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnQty = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.AllowUserToAddRows = false;
+            this.dgvProducts.AllowUserToDeleteRows = false;
+            this.dgvProducts.AllowUserToResizeColumns = false;
+            this.dgvProducts.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.LightGray;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.Black;
+            this.dgvProducts.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvProducts.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvProducts.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvProducts.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvProducts.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(29)))), ((int)(((byte)(33)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Courier New", 9.75F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(29)))), ((int)(((byte)(33)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvProducts.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnProductLocationId,
+            this.columnProducRef,
+            this.columnProductName,
+            this.columnQty});
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Courier New", 9.75F);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvProducts.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvProducts.EnableHeadersVisualStyles = false;
+            this.dgvProducts.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(29)))), ((int)(((byte)(33)))));
+            this.dgvProducts.Location = new System.Drawing.Point(9, 119);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.ReadOnly = true;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Courier New", 9.75F);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvProducts.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvProducts.RowHeadersVisible = false;
+            this.dgvProducts.RowHeadersWidth = 25;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.Color.Black;
+            this.dgvProducts.RowsDefaultCellStyle = dataGridViewCellStyle5;
+            this.dgvProducts.RowTemplate.DefaultCellStyle.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvProducts.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvProducts.Size = new System.Drawing.Size(845, 515);
+            this.dgvProducts.TabIndex = 30;
+            // 
+            // lbLocation
+            // 
+            this.lbLocation.AutoSize = true;
+            this.lbLocation.BackColor = System.Drawing.Color.Transparent;
+            this.lbLocation.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLocation.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(29)))), ((int)(((byte)(33)))));
+            this.lbLocation.Location = new System.Drawing.Point(5, 50);
+            this.lbLocation.Name = "lbLocation";
+            this.lbLocation.Size = new System.Drawing.Size(120, 22);
+            this.lbLocation.TabIndex = 33;
+            this.lbLocation.Text = "lbLocation";
+            // 
+            // cbLocations
+            // 
+            this.cbLocations.BackColor = System.Drawing.Color.White;
+            this.cbLocations.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cbLocations.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbLocations.ForeColor = System.Drawing.Color.Black;
+            this.cbLocations.FormattingEnabled = true;
+            this.cbLocations.Location = new System.Drawing.Point(9, 75);
+            this.cbLocations.Name = "cbLocations";
+            this.cbLocations.Size = new System.Drawing.Size(217, 24);
+            this.cbLocations.TabIndex = 34;
+            this.cbLocations.SelectionChangeCommitted += new System.EventHandler(this.cbLocations_SelectionChangeCommitted);
+            // 
+            // ColumnProductLocationId
+            // 
+            this.ColumnProductLocationId.HeaderText = "ColumnProductLocationId";
+            this.ColumnProductLocationId.Name = "ColumnProductLocationId";
+            this.ColumnProductLocationId.ReadOnly = true;
+            this.ColumnProductLocationId.Visible = false;
+            // 
+            // columnProducRef
+            // 
+            this.columnProducRef.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.columnProducRef.HeaderText = "columnProducRef";
+            this.columnProducRef.Name = "columnProducRef";
+            this.columnProducRef.ReadOnly = true;
+            this.columnProducRef.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // columnProductName
+            // 
+            this.columnProductName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.columnProductName.HeaderText = "columnProductName";
+            this.columnProductName.Name = "columnProductName";
+            this.columnProductName.ReadOnly = true;
+            // 
+            // columnQty
+            // 
+            this.columnQty.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.columnQty.HeaderText = "columnQty";
+            this.columnQty.Name = "columnQty";
+            this.columnQty.ReadOnly = true;
             // 
             // DashboardUc
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.cbLocations);
+            this.Controls.Add(this.lbLocation);
+            this.Controls.Add(this.dgvProducts);
             this.Font = new System.Drawing.Font("Courier New", 9.75F);
             this.ForeColor = System.Drawing.Color.White;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "DashboardUc";
             this.Size = new System.Drawing.Size(860, 643);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.DataGridView dgvProducts;
+        private System.Windows.Forms.Label lbLocation;
+        private System.Windows.Forms.ComboBox cbLocations;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnProductLocationId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnProducRef;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnProductName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnQty;
     }
 }
--- StockManager/Source/UserControls/DashboardUc.cs
+++ StockManager/Source/UserControls/DashboardUc.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using StockManager.Core.Source.Models;
+using StockManager.Services.Source;
+using StockManager.Source.Components;
+using StockManager.Translations.Source;
 
 namespace StockManager.Source.UserControls
 {
@@ -15,6 +15,60 @@ namespace StockManager.Source.UserControls
         public DashboardUc()
         {
             InitializeComponent();
+            SetTranslatedPhrases();
+            LoadDataAsync().Wait();
+        }
+
+        private void SetTranslatedPhrases()
+        {
+            lbLocation.Text = Phrases.GlobalLocation;
+
+            dgvProducts.Columns[1].HeaderText = Phrases.GlobalReference;
+            dgvProducts.Columns[2].HeaderText = Phrases.GlobalProduct;
+            dgvProducts.Columns[3].HeaderText = Phrases.StockMovementQty;
+        }
+
+        private async Task LoadDataAsync()
+        {
+            Spinner.InitSpinner();
+
+            IEnumerable<Location> locations = await AppServices.LocationService.GetAllAsync();
+            List<Location> filteredLocations = locations.Where(x => !x.IsMain).ToList();
+
+            cbLocations.DataSource = filteredLocations;
+            cbLocations.ValueMember = "LocationId";
+            cbLocations.DisplayMember = "Name";
+
+            await LoadProductsTableDataAsync(filteredLocations.ElementAt(0).LocationId);
+
+            Spinner.StopSpinner();
+        }
+
+        private async Task LoadProductsTableDataAsync(int locationId)
+        {
+            dgvProducts.Rows.Clear();
+
+            IEnumerable<ProductLocation> pLocations = await AppServices.ProductLocationService
+                   .FindAllByLocationIdAsync(locationId);
+
+            pLocations?.ToList().ForEach((plocation) => dgvProducts.Rows.Add(
+                plocation.ProductLocationId,
+                plocation?.Product?.Reference,
+                plocation?.Product?.Name,
+                "TODO: Add input" // TODO: Add input here
+            ));
+        }
+
+        private async void cbLocations_SelectionChangeCommitted(object sender, System.EventArgs e)
+        {
+            Location location = ( Location )cbLocations.SelectedItem;
+
+            if (location != null)
+            {
+                Spinner.InitSpinner();
+                await LoadProductsTableDataAsync(location.LocationId);
+                Spinner.StopSpinner();
+            }
         }
     }
 }
