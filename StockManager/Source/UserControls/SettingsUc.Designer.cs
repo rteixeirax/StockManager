55b611ee33be632a1e1adb15748cc0549fdcb740
Ricardo Teixeira <rteixxeira86@gmail.com>
2020-09-07 15:51:55 +0100

    Wip: Add languages combo to the SettingsUc

--- StockManager/Source/UserControls/SettingsUc.Designer.cs
+++ StockManager/Source/UserControls/SettingsUc.Designer.cs
@@ -28,22 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-      this.SuspendLayout();
-      //
-      // SettingsUserControl
-      //
-      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.BackColor = System.Drawing.SystemColors.Control;
-      this.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.ForeColor = System.Drawing.Color.White;
-      this.Margin = new System.Windows.Forms.Padding(4);
-      this.Name = "SettingsUserControl";
-      this.Size = new System.Drawing.Size(860, 420);
-      this.ResumeLayout(false);
+            this.lbLanguage = new System.Windows.Forms.Label();
+            this.cbLanguage = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lbLanguage
+            // 
+            this.lbLanguage.AutoSize = true;
+            this.lbLanguage.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLanguage.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(118)))), ((int)(((byte)(185)))));
+            this.lbLanguage.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lbLanguage.Location = new System.Drawing.Point(12, 17);
+            this.lbLanguage.Name = "lbLanguage";
+            this.lbLanguage.Size = new System.Drawing.Size(88, 16);
+            this.lbLanguage.TabIndex = 10;
+            this.lbLanguage.Text = "lbLanguage";
+            // 
+            // cbLanguage
+            // 
+            this.cbLanguage.BackColor = System.Drawing.Color.White;
+            this.cbLanguage.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cbLanguage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbLanguage.ForeColor = System.Drawing.Color.Black;
+            this.cbLanguage.FormattingEnabled = true;
+            this.cbLanguage.Location = new System.Drawing.Point(15, 36);
+            this.cbLanguage.Name = "cbLanguage";
+            this.cbLanguage.Size = new System.Drawing.Size(252, 24);
+            this.cbLanguage.TabIndex = 9;
+            // 
+            // SettingsUc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.lbLanguage);
+            this.Controls.Add(this.cbLanguage);
+            this.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "SettingsUc";
+            this.Size = new System.Drawing.Size(860, 420);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbLanguage;
+        private System.Windows.Forms.ComboBox cbLanguage;
     }
 }
--- StockManager/Source/UserControls/SettingsUc.cs
+++ StockManager/Source/UserControls/SettingsUc.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows.Forms;
+﻿using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using StockManager.Core.Source;
+using StockManager.Services.Source;
+using StockManager.Source.Components;
+using StockManager.Translations.Source;
 
 namespace StockManager.Source.UserControls
 {
@@ -7,8 +13,26 @@ namespace StockManager.Source.UserControls
         public SettingsUc()
         {
             InitializeComponent();
+            SetTranslatedPhrases();
+            LoadSettingsAsync().Wait();
         }
 
-        // TODO: Add content
+        private void SetTranslatedPhrases()
+        {
+            lbLanguage.Text = Phrases.GlobalLanguage;
+        }
+
+        private async Task LoadSettingsAsync()
+        {
+            Spinner.InitSpinner();
+
+            // Populate the language combo
+            cbLanguage.DataSource = AppConstants.AppLanguages;
+            cbLanguage.ValueMember = "Code";
+            cbLanguage.DisplayMember = "Name";
+            cbLanguage.SelectedItem = await AppServices.AppSettingsService.GetAppLanguageAsync();
+
+            Spinner.StopSpinner();
+        }
     }
 }
