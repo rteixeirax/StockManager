6a84883b4a92f53f026f27708ec3b490ba9ac0cf
Ricardo Teixeira <rteixxeira86@gmail.com>
2020-09-19 12:31:07 +0100

    Add a new type "ExportData"

---
+++ StockManager.Core/Source/Types/ExportData.cs
@@ -0,0 +1,14 @@
+﻿namespace StockManager.Core.Source.Types
+{
+    public class ExportData<TData, TOptions>
+    {
+        public TData Data { get; private set; }
+        public TOptions Options { get; private set; }
+
+        public ExportData(TData data, TOptions options)
+        {
+            Data = data;
+            Options = options;
+        }
+    }
+}
